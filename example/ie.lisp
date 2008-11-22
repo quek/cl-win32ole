b14dea2285c7fba1c6579955557fd772785f21a9
quek <quek@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-22 11:29:18 +0000

    remove example1, now dispatch is not funcallable-object.
    git-svn-id: http://svn.coderepos.org/share/lang/commonlisp/cl-win32ole/trunk@24647 d0d07461-0603-4401-acd4-de1884942a52

--- example/ie.lisp
+++ example/ie.lisp
@@ -1,17 +1,6 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (require :cl-win32ole)
+  (asdf:oos 'asdf:load-op :cl-win32ole)
   (use-package :cl-win32ole))
-
-(defun ie-example1 ()
-  (let ((ie (create-object "InternetExplorer.Application")))
-    (with-slots (visible busy document) ie
-      (setf visible t)
-      (funcall ie :navigate "http://sbcl.sourceforge.net/")
-      (loop while busy
-         do (sleep 0.5))
-      (format t "document title is \"~a\".~%" (slot-value document :title))
-      (sleep 3))
-    (funcall ie :quit)))
 
 (defun ie-example2 ()
   (let ((ie (create-object "InternetExplorer.Application")))
@@ -30,5 +19,5 @@
       (invoke ie :navigate "http://www.yahoo.co.jp/")
       (loop while busy do (sleep 0.5))
       (with-slots (all) document
-        (p (invoke all :tags "A")))))
-  )
+        (print (invoke all :tags "A")))
+      (ole ie :quit))))
